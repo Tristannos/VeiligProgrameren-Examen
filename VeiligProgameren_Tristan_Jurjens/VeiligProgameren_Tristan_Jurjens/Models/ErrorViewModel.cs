3718f24ff4923afd10a33e4669dda4b3f0e3f4e8
Tristannos <55278228+Tristannos@users.noreply.github.com>
2021-11-23 14:35:05 +0100

    Initial Push

---
+++ VeiligProgameren_Tristan_Jurjens/VeiligProgameren_Tristan_Jurjens/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VeiligProgameren_Tristan_Jurjens.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
